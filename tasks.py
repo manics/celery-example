6d7aefcdb7743e7969d7d0634c5765cd1d956cd9
Simon Li <orpheus+devel@gmail.com>
2017-02-22 17:58:16 +0000

    run docker via docker client library

--- tasks.py
+++ tasks.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-import sys
+import argparse
 import os
+import sys
+import docker
 import errno
-import subprocess
 from random import uniform
 
 from celery import Celery
@@ -31,29 +32,61 @@ def mkdir_p(path):
 
 # http://docs.celeryproject.org/en/latest/userguide/tasks.html
 @app.task(bind=True)
-def run(self, args):
-    out_fn, err_fn = args.pop(0), args.pop(0)
-    for fn in out_fn, err_fn:
-        mkdir_p(os.path.dirname(os.path.abspath(fn)))
-    with open(out_fn, "w") as fo, open(err_fn, "w") as fe:
-        ret = subprocess.call(args, stdout=fo, stderr=fe)
-    LOGGER.info("stdout: %s" % out_fn)
-    LOGGER.info("stderr: %s" % err_fn)
-    r = {'id': self.request.id, 'rc': ret}
-    if ret:
+def run(self, image, command, logout=None, logerr=None, inputpath=None):
+    client = docker.from_env()
+    kwargs = dict(
+        command=command,
+        detach=False,
+        name='run-%s' % self.request.id,
+        remove=True,
+        stderr=True,
+        stdout=True,
+    )
+    # kwargs['cpu_shares'] = 1
+
+    if inputpath:
+        if not os.path.isabs(inputpath) or not os.path.exists(inputpath):
+            raise ValueError(
+                'inputpath must be an existing absolute path: %s' % inputpath)
+        kwargs['volumes'] = {inputpath: {'bind': '/input', 'mode': 'ro'}}
+
+    for fn in logout, logerr:
+        if fn:
+            mkdir_p(os.path.dirname(os.path.abspath(fn)))
+    LOGGER.info("stdout: %s" % logout)
+    # LOGGER.info("stderr: %s" % logerr)
+
+    try:
+        output = client.containers.run(image, **kwargs)
+    except docker.errors.ContainerError as e:
         # Exponential backoff + jitter
         delay_base = app.conf.get('CUSTOM_RETRY_DELAY', 1)
         delay = int(uniform(2, 4) ** self.request.retries * delay_base)
-        raise self.retry(countdown=delay, exc=Exception(r))
+        raise self.retry(countdown=delay, exc=e)
+
+    with open(logout, 'w') as f:
+        f.write(output)
+
+    r = {'id': self.request.id, 'output': output}
     return r
 
 
 def main(argv):
-    if len(argv) < 4:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', help='Verbose output')
+    parser.add_argument('--inputpath',
+                        help='Input path (mounted read-only as /input)')
+    parser.add_argument('--out', help='Output stdout log file')
+    # parser.add_argument('--err', help='Output stderr log file')
+    parser.add_argument('image', help='Docker image')
+    parser.add_argument('commands', nargs='*', help='Command arguments')
+
+    args = parser.parse_args(argv[1:])
+    if args.verbose:
         print app.conf.humanize(with_defaults=False, censored=True)
-        sys.stderr.write('USAGE: python %s OUT ERR CMD\n' % argv[0])
-        sys.exit(2)
-    return run.delay(args=argv[1:])
+    return run.delay(
+        args.image, args.commands, logout=args.out, inputpath=args.inputpath)
 
 
 if __name__ == "__main__":
